d26190dc0af42ef3e9c86d0159e57122cb8037a7
varshithab-bms <varshithab.cs23@bmsce.ac.in>
2025-07-03 22:11:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const profileRoutes = require('./routes/profileRoutes');
 const app = express();
   const corsOptions = {
   app.use(cors({
-  origin: ['http://localhost:5173', 'https://expenaa-e1id.vercel.app'],
+  origin: ['http://localhost:5173', 'https://expenaaa.vercel.app/'],
   credentials: true
 }));
 
